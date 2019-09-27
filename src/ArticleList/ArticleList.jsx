995d1726bb21421a7dee2b5f5bdf42906dc5af6e
QiuyiZhang <zhang.qiuyi@husky.neu.edu>
2019-09-27 11:48:36 -0700

    assignment4

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -1,17 +1,26 @@
-import React from "react";
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import ArticleListItem from './ArticleListItem';
+import SlugClick from './SlugClick';
 
 const ArticleList = props => {
   return (
     <ul>
       {props.articles.map(article => (
-        <li key={article.slug}>ArticleListItem component goes here</li>
+        <li key={article.slug}>
+        <p>title={article.title}</p>
+        <p>shortText={article.shortText}</p>
+        <p>pubDate={article.pubDate}</p>
+        <ArticleListItem article  = {article}/>
+        </li>
       ))}
     </ul>
   );
 };
 
+
 ArticleList.propTypes = {
-  articles: PropTypes.array.isRequired
+  article: PropTypes.array.isRequired
 };
+
 export default ArticleList;
---
+++ src/ArticleList/ArticleListItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ArticleListItem = props => {
+  return (
+    <div>
+      <h3>{props.article.title}</h3>
+      <p>{props.article.shortText}</p >
+      <time dateTime={props.article.pubYear}>{props.article.pubDate}</time>
+      <button onClick={() => alert(props.article.slug)}>
+        show article slug
+      </button>
+    </div>
+  );
+};
+
+export default ArticleListItem;
